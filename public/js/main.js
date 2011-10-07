43789db170427b7a2903157dd1955fa6838c79d4
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-10-08 03:56:49 +0530

    SocketIO communicator added ClientSide helper class created

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+//Core Communication Functions
+
+function SMS() {
+	
+	var events = new EventEmitter2();
+	/**
+		Send Mobile Originated Message
+	*/
+
+
+	this.sendMo = function sendMo(myAddress, message) {
+		
+		socket.emit('MO', appId, myAddress, message);
+	}
+
+	/**
+	Handling MT messages
+	*/
+	var onSmsCallbacks = [];
+	var onBroadcastCallbacks = [];
+
+	this.on = function() {
+		events.on.apply(events, arguments);	
+	};
+
+	socket.on(appId, function(toAddress, message) {
+
+		if(toAddress == 'all_registered') {
+			//broadcast
+			events.emit('broadcast', message);
+		} else {
+			events.emit(toAddress, message);
+		}
+	});
+}
+
+var sms = new SMS();
