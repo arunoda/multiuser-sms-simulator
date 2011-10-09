7f37e79e0a8576f5079a1d1024e9620828446d41
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-10-09 11:55:32 +0530

    SMS store added

--- public/js/main.js
+++ public/js/main.js
@@ -35,3 +35,127 @@ function SMS() {
 		}
 	});
 }
+
+
+// SMS Store
+
+/**
+	No of messages in the store
+*/
+function SmsStore(limit) {
+	
+	limit = (limit)? limit : 20;
+
+	var all = generateArray();
+	var broadcasts = generateArray();
+	var phones = {};
+
+	this._all = all;
+	this._broadcasts = broadcasts;
+	this._phones = phones;
+
+	this.addSmsMo = function(to, message) {
+		
+		addSms(to, message, 'MO');
+		addToAll(to, message, 'MO');
+	};
+
+	this.addSmsMt = function(to, message) {
+		
+		addSms(to, message, 'MT');
+		addToAll(to, message, 'MT');
+	};
+
+	this.addBroadcast = function(message) {
+		
+		broadcasts.unshift({
+			message: message,
+			address: 'broadcast',
+			type: 'MT',
+			time: new Date().getTime()
+		});
+		broadcasts.pop();
+
+		addToAll('broadcast', message, 'MT');
+
+		//add broadcast to each phoneNo
+		for(var phoneNo in phones) {
+			phones[phoneNo].unshift({
+				message: message,
+				address: 'broadcast',
+				type: 'MT',
+				time: new Date().getTime()
+			});
+			phones[phoneNo].pop();
+		}
+	};
+
+	/**
+		@param phoneNo - phoneNo which we need to get SMS
+		@return all the messages for the given phoneNo or give all if phoneNo is null
+	*/
+	this.getSmsFor = function(phoneNo) {
+		
+		if(!phoneNo) {
+			return cleanMessages(all);
+		} else if (phones[phoneNo]) {
+			return cleanMessages(phones[phoneNo]);
+		} else {
+			return [];
+		}
+	};
+
+	function addSms(to, message, type) {
+
+		if(!phones[to]) {
+			phones[to] = generateArray();	
+		}
+
+		phones[to].unshift({
+			message: message,
+			address: to,
+			type: type,
+			time: new Date().getTime()
+		});
+
+		phones[to].pop();
+	};
+
+	function addToAll(to, message, type) {
+		
+		all.unshift({
+			message: message,
+			address: to,
+			type: type,
+			time: new Date().getTime()
+		});	
+		all.pop();
+	};
+
+	/**
+		Remove nulls and undefined in an array
+		and return an new array
+	*/
+	function cleanMessages(arr) {
+
+		var arrNew = [];
+		for(var index in arr) {
+
+			if(arr[index]) {
+				arrNew.push(arr[index]);
+			} else {
+				return arrNew;
+			}
+		}
+	}
+
+	function generateArray() {
+		
+		var arr = [];
+		for(var lc=0; lc<limit; lc++) {
+			arr.push(undefined);
+		}
+		return arr;
+	}
+
+}
